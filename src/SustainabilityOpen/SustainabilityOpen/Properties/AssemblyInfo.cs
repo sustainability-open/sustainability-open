e9e2aaed00adb0a3ac2110b890438e8876fcfb97
Jeroen Coenders <jeroencoenders@gmail.com>
2013-01-03 19:03:18 +0100

    Make some improvements to the assembly info and build process

--- src/SustainabilityOpen/SustainabilityOpen/Properties/AssemblyInfo.cs
+++ src/SustainabilityOpen/SustainabilityOpen/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SustainabilityOpen")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SustainabilityOpen framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BEMNext Lab and contributors")]
 [assembly: AssemblyProduct("SustainabilityOpen")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
