dc15bac504b8992f0fa7f640903ef0e37e66c2c2
Jeroen Coenders <jeroencoenders@gmail.com>
2013-01-03 19:36:22 +0100

    Added a beam and a designer to the simple components example

---
+++ documentation/examples/SimpleComponentsExample/SimpleComponentsExample/Beam_HE200A.cs
@@ -0,0 +1,30 @@
+﻿// This file implements a steel HE200A beam
+
+// Note the inclusion of the framework in the reference
+using SustainabilityOpen.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// The namespace can be whatever you want it to be
+namespace SimpleComponentsExample
+{
+    /// <summary>
+    /// Class overrides the SOPhysicalObject class
+    /// </summary>
+    public class Beam_HE200A : SOPhysicalObject
+    {
+        /// <summary>
+        /// Constructor
+        /// Note the name of the physical object you will need to pass to the base class.
+        /// </summary>
+        public Beam_HE200A()
+            : base("HE200A")
+        {
+            // Adding a material quantity to the beam of 1 m3 of the material steel.
+            this.AddMaterialQuantity(new SOMaterial("steel"), 1, "m3");
+        }
+
+    }
+}
---
+++ documentation/examples/SimpleComponentsExample/SimpleComponentsExample/SimpleDesignerExample.cs
@@ -0,0 +1,37 @@
+﻿/// This file implements a simple designer
+
+/// Note the inclusion of the framework in the reference
+using SustainabilityOpen.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// The namespace can be whatever you want it to be
+namespace SimpleComponentsExample
+{
+    /// <summary>
+    /// Class overrides from the SODesigner class
+    /// </summary>
+    public class SimpleDesignerExample : SODesigner
+    {
+        /// <summary>
+        /// Constructor
+        /// Note the name assessment you will need to pass to the base class.
+        /// </summary>
+        public SimpleDesignerExample()
+            : base("simple_designer_0001")
+        {
+        }
+        /// <summary>
+        /// Overriding the RunDesigner method.
+        /// This method will be run by the framework to make the design.
+        /// </summary>
+        public override void RunDesigner()
+        {
+            // Adding two beams with the designer
+            this.AddObject(new Beam_HE200A());
+            this.AddObject(new Beam_HE200A());
+        }
+    }
+}
