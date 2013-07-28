b01b8f72c84c3a5059b04c6b2f1bf5efc06ca80e
jeroencoenders <jeroencoenders@gmail.com>
2013-07-28 21:32:40 +0200

    Moved design objects into a Design directory and modified namespace of SOMaterial

---
+++ src/SustainabilityOpen/SustainabilityOpen/Framework/Design/SOMaterial.cs
@@ -0,0 +1,47 @@
+﻿/// Copyright 2012-2013 Delft University of Technology, BEMNext Lab and contributors
+/// 
+///    Licensed under the Apache License, Version 2.0 (the "License");
+///    you may not use this file except in compliance with the License.
+///    You may obtain a copy of the License at
+/// 
+///        http://www.apache.org/licenses/LICENSE-2.0
+/// 
+///    Unless required by applicable law or agreed to in writing, software
+///    distributed under the License is distributed on an "AS IS" BASIS,
+///    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///    See the License for the specific language governing permissions and
+///    limitations under the License.
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SustainabilityOpen.Framework.Design
+{
+    /// <summary>
+    /// Material class
+    /// </summary>
+    public class SOMaterial
+    {
+        private string m_Name;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Name of the material</param>
+        public SOMaterial(string name)
+        {
+            this.m_Name = name;
+        }
+
+        /// <summary>
+        /// Returns the name of the material
+        /// </summary>
+        public string Name
+        {
+            get { return this.m_Name; }
+        }
+    }
+}
---
+++ src/SustainabilityOpen/SustainabilityOpen/Framework/Design/SOMaterialQuantity.cs
@@ -0,0 +1,50 @@
+﻿/// Copyright 2012-2013 Delft University of Technology, BEMNext Lab and contributors
+/// 
+///    Licensed under the Apache License, Version 2.0 (the "License");
+///    you may not use this file except in compliance with the License.
+///    You may obtain a copy of the License at
+/// 
+///        http://www.apache.org/licenses/LICENSE-2.0
+/// 
+///    Unless required by applicable law or agreed to in writing, software
+///    distributed under the License is distributed on an "AS IS" BASIS,
+///    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///    See the License for the specific language governing permissions and
+///    limitations under the License.
+/// 
+
+using SustainabilityOpen.Framework.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SustainabilityOpen.Framework
+{
+    public class SOMaterialQuantity
+    {
+        private SOMaterial m_Material;
+        private double m_Quantity;
+        private string m_Unit;
+
+        public SOMaterialQuantity(SOMaterial material, double quantity, string unit)
+        {
+            this.m_Material = material;
+            this.m_Quantity = quantity;
+            this.m_Unit = unit;
+        }
+        public SOMaterial Material
+        {
+            get { return this.m_Material; }
+        }
+        public double Quantity
+        {
+            get { return this.m_Quantity; }
+            set { this.m_Quantity = value; }
+        }
+        public string Unit
+        {
+            get { return this.m_Unit; }
+        }
+    }
+}
