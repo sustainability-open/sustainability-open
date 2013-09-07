731cc704c5cbf9d9fb0a25574d21427152d6609b
jeroencoenders <jeroencoenders@gmail.com>
2013-09-07 19:15:06 +0200

    Added SOGrasshopperController to control framework in Grasshopper

---
+++ src/grasshopper/SustainabilityOpen.Grasshopper/SOGrasshopperController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SustainabilityOpen.Grasshopper
+{
+    public sealed class SOGrasshopperController
+    {
+        private static volatile SOGrasshopperController m_Instance;
+        private static object m_SyncRoot = new Object();
+
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static SOGrasshopperController Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    lock (m_SyncRoot)
+                    {
+                        if (m_Instance == null)
+                        {
+                            m_Instance = new SOGrasshopperController();
+                        }
+                    }
+                }
+                return m_Instance;
+            }
+        }
+
+    }
+}
